1aa35d9630f6cf89a01659445cc56398064be056
Rama-Qandeel <rama.qndeel@gmail.com>
2020-11-28 14:48:00 +0200

    create popup window

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-
+import PopUp from "./PopUp";
 const Information = (props) => {
   // console.log("props ", props.location.state);
-  const [favorite, setFavorite] = useState([]);
   const [watch, setWatch] = useState([]);
-const [ popup,setPopup]=useState(false);
+  const [seen, setSeen] = useState(false);
   const {
     title,
     poster_path,
@@ -16,25 +15,11 @@ const [ popup,setPopup]=useState(false);
     original_language,
   } = props.location.state;
 
-
- const  togglePopup=() =>{
-  setPopup(!popup)
-}
-
-  const addFavorite = () => {
-    if (!localStorage.getItem("favourites")) {
-      const movie = [];
-      movie.push(props.location.state);
-      localStorage.setItem("favourites", JSON.stringify(movie));
-    } else {
-      const movies = JSON.parse(localStorage.getItem("favourites"));
-      movies.push(props.location.state);
-      localStorage.setItem("favourites", JSON.stringify(movies));
-    }
-    setFavorite(JSON.parse(localStorage.getItem("favourites")));
+  const togglePop = () => {
+    setSeen(!seen);
   };
 
-  
+
   const addWatchList = () => {
     if (!localStorage.getItem("watch")) {
       const movie = [];
@@ -48,47 +33,75 @@ const [ popup,setPopup]=useState(false);
     setWatch(JSON.parse(localStorage.getItem("watch")));
   };
 
-
   return (
     <div className="result">
       <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
 
       <div className="info">
-        <p> <strong>Title : </strong> {title} </p> 
-        
+        <p>
+          {" "}
+          <strong>Title : </strong> {title}{" "}
+        </p>
+
         <div>
-        {" "}
-        <p> <strong>Original Language :</strong> {original_language}</p>
+          {" "}
+          <p>
+            {" "}
+            <strong>Original Language :</strong> {original_language}
+          </p>
+        </div>
+        <div>
+          {" "}
+          <p>
+            {" "}
+            <strong>Overview : </strong>
+            {overview}
+          </p>
+        </div>
+
+        <div>
+          {" "}
+          <span>
+            {" "}
+            <strong> Popularity : </strong>
+            {popularity}
+          </span>
+        </div>
+        <div>
+          {" "}
+          <span>
+            {" "}
+            <strong> Vote Average :</strong> {vote_average}
+          </span>
+        </div>
+        <div>
+          {" "}
+          <span>
+            {" "}
+            <strong>Vote Count:</strong> {vote_count}
+          </span>
+        </div>
+        <div>
+          {" "}
+          <span>
+            {" "}
+            <strong> Release Date:</strong> {release_date}
+          </span>
+        </div>
+        <div>
+          {" "}
+          <div className="" onClick={togglePop}>
+            {/* <button className="btn-add"  onClick={addFavorite}>Favorite</button> */}
+            <button className="btn-add">Favorite</button>
+          </div>
+          {seen ? (
+            <PopUp toggle={togglePop} info={props.location.state} />
+          ) : null}
+          <button className="btn-add" onClick={addWatchList}>
+            Add to WatchList
+          </button>
+        </div>
       </div>
-      <div>
-        {" "}
-        <p> <strong>Overview : </strong>
-         {overview}</p>
-        
-      </div>
-    
-      <div>
-        {" "}
-        <span> <strong> Popularity : </strong>{popularity}</span>
-      </div>
-      <div>
-        {" "}
-        <span> <strong> Vote Average :</strong> {vote_average}</span>
-      </div>
-      <div>
-        {" "}
-        <span> <strong>Vote Count:</strong> {vote_count}</span>
-      </div>
-      <div>
-        {" "}
-        <span> <strong> Release Date:</strong> {release_date}</span>
-      </div>
-      <div>
-        {" "}
-        <button className="btn-add" onClick={addFavorite}>Favorite</button>
-        <button className="btn-add" onClick={addWatchList}>Add to WatchList</button>
-      </div>
-    </div>
     </div>
   );
 };
